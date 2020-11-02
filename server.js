ebfced789635ef69b6c117b7e3dae19edfb52e9e
Franco Ortega <francopdx@gmail.com>
2020-11-02 14:05:02 -0800

    add server.js

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+    console.log('console log test')
+    res.json([
+        {
+            name: 'lila'
+        },
+        {
+            name: 'devar'
+        },
+        {
+            name: 'ilan'
+        }
+    ]);
+});
+
+
+app.get('/stuff', (req, res) => {
+    console.log('console log test')
+    res.json([
+        {
+            name: 'skateboard'
+        },
+        {
+            name: 'dice'
+        },
+        {
+            name: 'kite'
+        }
+    ]);
+});
+
+
+app.listen(port, () => {
+    console.log(`Express app listening at http://localhost:${port}`);
+});
